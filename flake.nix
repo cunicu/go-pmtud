99b6d62e0eaded4259d996146455972f27a8de69
Steffen Vogel <post@steffenvogel.de>
2024-08-27 08:37:38 +0200

    Add Nix flake
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2023 Steffen Vogel <post@steffenvogel.de>
+# SPDX-License-Identifier: Apache-2.0
+{
+  description = "Gont: A Go testing framework for distributed applications";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell =
+          pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              golangci-lint
+              reuse
+            ];
+          };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
